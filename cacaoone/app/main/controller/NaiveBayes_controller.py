66cf3be42f0b0cc752ad7ba04c52c501162e9a09
VietAnhPhan <30721901+VietAnhPhan@users.noreply.github.com>
2019-05-23 19:53:25 +0700

    Create NaiveBayes_controller.py
    naivebayes_controller.py

---
+++ cacaoone/app/main/controller/NaiveBayes_controller.py
@@ -0,0 +1,37 @@
+from ..util.dto import NaiveBayesDto
+from flask_restplus import Resource
+from ..service.data_service import read_all_data_csv
+from flask import request,json
+from app.main.service.auth_helper import Auth
+from ..service.NaiveBayes_service import _GaussianNB
+from ..service.NaiveBayes_service import _MultinomialNB
+api=NaiveBayesDto.api
+
+@api.route('/GaussianNB')
+class GaussianNB(Resource):
+    @api.doc('Run alogorithm GaussianNB')
+    @api.response(201,'Completed')
+    def get(self):
+        response = Auth.get_logged_in_user(new_request=request)
+        user_profile = response[0].get('data')
+        id_user = user_profile.get('user_id')
+        result = read_all_data_csv(id_user)
+        dataset = result.get('data')
+        test_size=request.args.get('test_size', type=int)
+        random_state=request.args.get('random_state', type=float)
+        GaussNB=_GaussianNB(dataset, test_size, random_state)
+        return GaussNB.write_json()
+@api.route('/MultinomialNB')
+class MultinomialNB(Resource):
+    @api.doc('Run alogorithm MultinomialNB')
+    @api.response(201,'Completed')
+    def get(self):
+        response = Auth.get_logged_in_user(new_request=request)
+        user_profile = response[0].get('data')
+        id_user = user_profile.get('user_id')
+        result = read_all_data_csv(id_user)
+        dataset = result.get('data')
+        test_size=request.args.get('test_size', type=int)
+        random_state=request.args.get('random_state', type=float)
+        MultiNB=_MultinomialNB(dataset, test_size, random_state)
+        return MultiNB.write_json()
